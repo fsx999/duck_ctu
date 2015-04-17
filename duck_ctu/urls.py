ff01269aaaa4c75f3f0260ec7f055d1b732bc71a
paul guichon <paul.guichon@iedparis8.net>
2015-04-17 14:47:26 +0200

    ajout envoi mail auditeur

--- duck_ctu/urls.py
+++ duck_ctu/urls.py
@@ -11,22 +11,22 @@ from duck_inscription.views.enregistrement_views import UserView, EmailView
 
 urlpatterns = patterns(
     '',
-    url(r'^Pinscription/autocomplete/', include('autocomplete_light.urls')),
-    (r'^Pinscription/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html',
+    url(r'^autocomplete/', include('autocomplete_light.urls')),
+    (r'^$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html',
                                                 'authentication_form': LoginIED}),
-    (r'^Pinscription/compte/', include('duck_inscription.urls.enregistrement_urls')),
-    (r'^Pinscription/individu/', include('duck_inscription.urls.individu_urls')),
-    (r'^Pinscription/voeu/', include('duck_inscription.urls.wish_urls')),
-    url(r'^Pinscription/test_user/$', UserView.as_view(), name='test_user'),
-    url(r'^Pinscription/test_email/$', EmailView.as_view(), name='test_email'),)
+    (r'^compte/', include('duck_inscription.urls.enregistrement_urls')),
+    (r'^individu/', include('duck_inscription.urls.individu_urls')),
+    (r'^voeu/', include('duck_inscription.urls.wish_urls')),
+    url(r'^test_user/$', UserView.as_view(), name='test_user'),
+    url(r'^test_email/$', EmailView.as_view(), name='test_email'),)
 
 urlpatterns += patterns(
     '',
-    url(r'^Pinscription/captcha/', include('captcha.urls')),)
+    url(r'^captcha/', include('captcha.urls')),)
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += patterns(
         '',
-        url(r'^Pinscription/__debug__/', include(debug_toolbar.urls)),)
+        url(r'^__debug__/', include(debug_toolbar.urls)),)
     urlpatterns += staticfiles_urlpatterns()
