8c1d78dc90f9cc1d0b0cf83fbe73c9951743d5b1
paul guichon <paul.guichon@iedparis8.net>
2015-04-29 11:04:39 +0200

    mise à jour template

--- duck_ctu/bo_urls.py
+++ duck_ctu/bo_urls.py
@@ -1,15 +1,22 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from duck_inscription.views import UserView, EmailView
 import xadmin
 
-admin.autodiscover()
 xadmin.autodiscover()
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-                       url(r'^inscription/', include('duck_inscription.urls.adminx_urls')),
+                   url(r'^inscription/', include('duck_inscription.urls.adminx_urls')),
+
+                    url(r'^autocomplete/', include('autocomplete_light.urls')),
+                    url(r'^compte/', include('duck_inscription.urls.enregistrement_urls')),
+                    url(r'^individu/', include('duck_inscription.urls.individu_urls')),
+                    url(r'^voeu/', include('duck_inscription.urls.wish_urls')),
+                    url(r'^test_user/$', UserView.as_view(), name='test_user'),
+                    url(r'^test_email/$', EmailView.as_view(), name='test_email'),
                        url(r'^utils/', include('duck_utils.urls')),
                        url(r'admin/', include(admin.site.urls)),
                        url(r'^', include(xadmin.site.urls)),
